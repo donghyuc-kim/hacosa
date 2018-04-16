e5834e0733010bc3f9e5da7e233c3430e66640c8
donghyuc-kim <kkimluv+a@gmail.com>
2018-04-16 16:35:12 +0900

    Update ex.js

--- 227030/ex.js
+++ 227030/ex.js
@@ -1,4 +1,5 @@
 // case1 
+// arguments 라는 이름으로 파라메터를 배열처럼 사용할 수 있습니다. 
 
 function toggle() {
     console.log(arguments[0]);
@@ -10,6 +11,7 @@ toggle(1,2,3,4,5,6)
 
 
 // case2 
+// 필요에 따라서는 JSON을 만들어 넘기면 한개 이상일 경우 유용하게 사용할 수 있기도 합니다. 
 
 function toggle(obj) {
     if ( 'a' in obj ) {
