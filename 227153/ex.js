85be19346e2b3c045ba4a5962aae121392a0197a
donghyuc-kim <kkimluv@gmail.com>
2018-04-17 16:21:59 +0900

    답변 등록

---
+++ 227153/ex.js
@@ -0,0 +1,8 @@
+function setValueInitial() {
+    header.style.bottom = 'initial';
+    footer.style.top = 'initial';
+    sidebarLeft.style.right = 'initial';
+    sidebarRight.style.left = 'initial';    
+}
+
+setValueInitial();
