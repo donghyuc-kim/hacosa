684660d8a60144738018f097bcecf60b124af7d7
donghyuc-kim <kkimluv@gmail.com>
2018-04-24 10:16:11 +0900

    예제 등록

---
+++ 227543/ex.js
@@ -0,0 +1,31 @@
+function c() {
+    try {
+        xxx
+    } catch(e) {
+        // throw e;
+        // output : ReferenceError: xxx is not defined
+
+        // throw new Error('Error가 발생했습니다.');
+        // output : Error: Error가 발생했습니다.
+
+        return e.message;
+        // output : cv xxx is not defined
+    } finally {
+        return 'return-c-finally';
+        // output : cv return-c-finally
+    }
+
+    return 'return-c'; 
+}
+
+function b() {
+    var cv = c(); 
+
+    console.log('cv', cv);
+}
+
+function a() {
+    b(); 
+}
+
+a();
