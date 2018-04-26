2fd4fe913d369f0e5e1b8f610344d6f1e6fe75ae
donghyuc-kim <kkimluv@gmail.com>
2018-04-26 10:39:32 +0900

    promise.then 이후에 데이타 처리

---
+++ 227813/ex.js
@@ -0,0 +1,20 @@
+let result; 
+
+console.log('1'); 
+
+axios
+    .get('/user/12345')
+    .then(function(response) {
+        // result = response;
+
+        then이후에result처리( response ); 
+
+        console.log('3'); 
+    });
+
+console.log('2');
+
+function then이후에result처리( data ) {
+    console.log('4');
+    console.log(data); 
+}  
