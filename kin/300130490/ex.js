2d1e9243bda12618bc6ae5e56c41579d5c008ca0
donghyuc-kim <kkimluv@gmail.com>
2018-04-26 13:33:38 +0900

    JSON에서 특정 프로퍼티 값을 얻어내는 방법

---
+++ kin/300130490/ex.js
@@ -0,0 +1,15 @@
+var data = {
+    "booklist":[
+        {"image":"https://prefffsco/bC5ELQ/alex_min.png"}, 
+        {"image":"https://prefffsco/bC5ELQ/alex_min.png"}, 
+        {"image":"https://prefffsco/bC5ELQ/alex_min.png"}, 
+        {"image":"https://prefffsco/bC5ELQ/alex_min.png"}, 
+        {"image":"https://prefffsco/bC5ELQ/alex_min.png"}
+    ]
+}; 
+
+
+for ( var i = 0; i < data.booklist.length; i++ ) {
+    console.log(data.booklist[i].image); 
+    // create , append 실행 
+}
