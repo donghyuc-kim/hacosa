334f7949ced14ec7ba8e74f1b6d356d820b0f23f
donghyuc-kim <kkimluv@gmail.com>
2018-04-26 13:13:13 +0900

    prototype과 null 관련 질문

---
+++ kin/300160568/ex.js
@@ -0,0 +1,20 @@
+function Person(name){
+  this.name = name;
+  this.introduce = function(){
+    return 'My name is '+this.name;
+  }
+}
+
+Person.prototype.name=null;
+Person.prototype.introduce=null;
+
+function Programmer(name){
+  this.name = name;
+}
+
+Programmer.prototype = new Person();
+var p1 = new Programmer('egoing');
+
+
+console.log(p1);
+console.log(p1.introduce());
