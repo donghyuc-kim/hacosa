f1ac8bf4d63df97f24838b3702821ffba9a03b11
donghyuc-kim <kkimluv@gmail.com>
2018-04-26 10:41:00 +0900

    .

--- 227813/ex.js
+++ 227813/ex.js
@@ -17,4 +17,13 @@ console.log('2');
 function then이후에result처리( data ) {
     console.log('4');
     console.log(data); 
+
+    result = data; 
+
+    또다른처리(); 
 }  
+
+function 또다른처리() {
+    console.log('5'); 
+    console.log(JSON.stringify(result)); 
+}
